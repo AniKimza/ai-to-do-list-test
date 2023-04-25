bb210fda0aa64646ceeab87fca0db93cbea88e6f
AniKimza <ani.kimza@gmail.com>
2023-04-25 21:49:39 +0200

    Fix ui and ad animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,57 @@
-import { Stack } from "@mui/system"
-import { ArticleCard } from "./Card"
-import { Grid, TextInput, Container, SimpleGrid, } from "@mantine/core"
-import { useState } from "react"
-
+import { Stack } from "@mui/system";
+import { ArticleCard } from "./assets/Card/ArticleCard";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 
 function App() {
 
+  const [request, setRequest] = useState('');
 
-const [cardTitle, setCardTitle] = useState('');
-
+  const handleRequest = (e) => {
+    setRequest(e.target.value);
+  };
 
   return (
-    <>
-     <Stack justifyContent={"center"} height={'100vh'}>
-      <Container my='lg' >
-        <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'lg', cols: 1 }]}>
-          <Grid gutter="md">
-            <Grid.Col span={8}>
-              <TextInput
-                onChange={e => setCardTitle(e.target.value)}
-                value={cardTitle}
-                label="Write"
-                mt="md"
-                autoComplete="nope"
-              />
-            </Grid.Col>
-          </Grid>
-          <Grid>
-            <Grid.Col span={10}>
-              <ArticleCard
-                image="https://i.imgur.com/Cij5vdL.png"
-                link="https://mantine.dev/"
-                title={cardTitle}
-                rating="outstanding"
-                description="Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires."
-                author={{
-                  "name": "Bill Wormeater",
-                  "image": "https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80"
-                }}
-              />
-            </Grid.Col>
-          </Grid>
-        </SimpleGrid>
-      </Container>
+    
+      <Stack height='100vh' direction="row" justifyContent="center" alignItems='center' spacing={3}>
+         <motion.div
+    initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.3,
+        ease: [0, 0.71, 0.2, 1.01],
+        scale: {
+          type: "spring",
+          damping: 5,
+          stiffness: 100,
+          restDelta: 0.001
+        }
+      }}
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+  >
+        <TextField onChange={handleRequest} />
+        </motion.div>
+        <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.3,
+        ease: [0, 0.71, 0.2, 1.01],
+        scale: {
+          type: "spring",
+          damping: 5,
+          stiffness: 100,
+          restDelta: 0.001
+        }
+      }}
+    >
+        <ArticleCard request={request} />
+        </motion.div>
       </Stack>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
